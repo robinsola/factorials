fe8dafc36fafebca9aeecaa7aee137648471f0e2
Robin Sola <rsolarms@gmail.com>
2017-11-08 10:11:48 -0800

    take out forEach loop and fix for loop with new numbers var.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
+// Business Logic:
+
 var numbers = 1;
+
+// User Logic:
+
 $(document).ready(function() {
   $("#input").submit(function(e) {
-
     e.preventDefault();
-    var numbers = [];
-    var number1 = parseInt($("#number").val());
-    for (var i=1; i <= number1; i++) {
-      numbers.push(i);
-      numbers.forEach(function(number) {
-        numbers = numbers * i;
-      });
-      console.log(numbers);
 
+    var number1 = parseInt($("#number").val());
+
+    for (var i=1; i <= number1; i++) {
+      numbers = numbers * i;
+    }
 
       $("#output").text(numbers);
-    }
   });
 });
